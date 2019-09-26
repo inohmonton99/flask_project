99523da428dd1eccb1f4f4ec566c06c9b946549e
inohmonton99 <inohfrancismonton@gmail.com>
2019-09-26 21:10:28 +0800

    script and README updated

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # flask_project
 simple api for video uploader in onepanel workspaces
+
+**COMMANDS:**
+```bash
+export FLASK_APP=server.py
+flask run
+or
+python server.py
+
+* Serving Flask app "server" (lazy loading)
+* Environment: production
+WARNING: This is a development server. Do not use it in a production deployment.
+Use a production WSGI server instead.
+* Debug mode: on
+* Restarting with windowsapi reloader
+* Debugger is active!
+* Debugger PIN: 230-078-479
+* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+```
+```bash
+curl http://localhost:5000/file -d "filename=test" -X PUT
+
+curl http://localhost:5000/
+{
+    "filename": "test"
+}
+
+
+```
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 # http://flask.pocoo.org/docs/patterns/fileuploads/
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
+from flask_restful import Resource, Api
 
 UPLOAD_FOLDER = 'uploads'
 
@@ -11,11 +12,13 @@ if UPLOAD_FOLDER not in os.getcwd():
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'flv', 'wmv'}
 
 app = Flask(__name__)
+api = Api(app)
 app.secret_key = "secret_key_54321"
 # directory to save uploaded videos
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # config to set maximum file size(current = 16mb)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+file_curl = {}
 
 
 def allowed_file(filename):
@@ -23,8 +26,20 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+class TodoSimple(Resource):
+    def put(self, todo_id):
+        file_curl[todo_id] = request.form['filename']
+        return {todo_id: file_curl[todo_id]}
+
+    def get(self, todo_id):
+        # TODO: load dict file as var=filename
+        # TODO: process upload with set filename
+        return {todo_id: file_curl[todo_id]}
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    # for local UI uploading
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -38,18 +53,19 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+            # file path for uploads
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        '''
 
 
 @app.route('/uploads/<filename>')
@@ -57,6 +73,13 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
